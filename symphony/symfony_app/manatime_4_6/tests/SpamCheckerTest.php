83edd1c262e306eced3985221869aa07e210840c
jeetendrahurloll <jeetendrag10@gmail.com>
2023-06-12 15:01:36 +0400

    before generating fake data

--- symphony/symfony_app/manatime_4_6/tests/SpamCheckerTest.php
+++ symphony/symfony_app/manatime_4_6/tests/SpamCheckerTest.php
@@ -8,6 +8,19 @@ class SpamCheckerTest extends TestCase
 {
     public function testSomething(): void
     {
+
+        for ($x = 0; $x <= 10; $x++) {
+
+            $this->assertTrue(true);
+
+        }
+
+    }
+
+    public function testAPI():void
+    {
         $this->assertTrue(true);
+
+
     }
 }
