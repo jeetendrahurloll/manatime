cc3e361b55a91ca1b7aa9665f44723f13aa8fdc8
jeetendrahurloll <jeetendrag10@gmail.com>
2023-06-15 08:18:02 +0400

    cleanued up api test boiler plate code

--- symphony/symfony_app/manatime_4_6/tests/SpamCheckerTest.php
+++ symphony/symfony_app/manatime_4_6/tests/SpamCheckerTest.php
@@ -6,16 +6,30 @@ use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Client;
 use PDO;
 
+
 class SpamCheckerTest extends TestCase
 {
 
+    public static $dbh=null;
+    //private $number = 0;
 
     /**
      *@beforeClass
      */
     public static function beforeClassFunction(): void
     {
-        echo ("\n before class function \n");
+        echo ("\n Before Class function \n");
+        /**
+         * Create a direct database connection.
+         * Direct connection without symfony is preferred to avoid complications.
+         */
+
+         //$DATABASE_URL = "mysql://root:mypswd@172.17.0.3:3306/app?serverVersion=8&charset=utf8mb4";
+         //$parts = parse_url($DATABASE_URL);
+         //print_r($parts); 
+         //echo ("db url   " . $_ENV["DATABASE_URL"]);
+        self::$dbh = new PDO('mysql:host=172.17.0.3;dbname=app', 'root', 'mypswd');
+
     }
 
 
@@ -24,65 +38,47 @@ class SpamCheckerTest extends TestCase
      */
     public static function afterClassFunction(): void
     {
-        echo ("\n after class function \n");
+        echo ("\n After class function \n");
+        /**
+         * cleanup pdo database connection
+         */
+         self::$dbh=null;
     }
 
     /**
      * @before
      */
-    public function someBefore(): void
-    {
-        echo ("\n somebefore \n");
+    public function beforeEachTest(): void
+    {        
+        echo ("\n Before each test \n");
+        /**
+         * populate database with test data
+         */
+        self::$dbh->query("INSERT INTO manatime_equipment (name,category,number,description,created_at,updated_at)VALUES ('keyboard',           'input device',     'sn656565',    'keyboard given to sanjeev','2023-06-13 12:23:45','2023-06-13 13:23:45')");
+        self::$dbh->query("INSERT INTO manatime_equipment (name,category,number,description,created_at,updated_at)VALUES ('mouse',              'input device',     'zx5ggtg5',    'given to Marie jo',        '2023-06-13 13:23:45','2023-06-14 13:23:45')");
+        self::$dbh->query("INSERT INTO manatime_equipment (name,category,number,description,created_at,updated_at)VALUES ('screen',             'display device',   'hyy656565',   'damaged unrepairable',     '2023-06-13 14:23:45','2023-06-15 13:23:45')");
+        self::$dbh->query("INSERT INTO manatime_equipment (name,category,number,description,created_at,updated_at)VALUES ('laptop',             'input device',     '09809807jh',  'reported malfunc,untested','2023-06-13 15:23:45','2023-06-16 13:23:45')");
+        self::$dbh->query("INSERT INTO manatime_equipment (name,category,number,description,created_at,updated_at)VALUES ('removable hard disk','input device',     'hkhjkgyt987', 'damaged by sanjeev',       '2023-06-13 16:23:45','2023-06-17 13:23:45')");
+
+
     }
 
 
     /**
      * @after
      */
-    public function someAfter(): void
+    public function afterEachTest(): void
     {
-        echo ("\n someAfter \n");
+        /**
+         * Cleans up database and resets it to blank
+         */
+        echo ("\n After each test \n");
+        self::$dbh->query("DELETE FROM manatime_equipment");
+
     }
 
 
-
-    /**
-     *@after
-     */
-    public function anotherAfter(): void
-    {
-        echo ("\n abotherAfter \n");
-    }
-
-
-
-
-
-
-    public function testSomething(): void
-    {
-        //$dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
-        $DATABASE_URL="mysql://root:mypswd@172.17.0.3:3306/app?serverVersion=8&charset=utf8mb4";
-        $dbh = new PDO('mysql:host=172.17.0.3;dbname=app', 'root', 'mypswd');
-        $parts = parse_url($DATABASE_URL);
-        print_r($parts);
-
-        echo("db url   ".$_ENV["DATABASE_URL"]);
-
-        // use the connection here
-        $dbh->query("INSERT INTO manatime_equipment (name,category,number,description,created_at,updated_at)VALUES ('keyboard',           'input device',     'sn656565',    'keyboard given to sanjeev','2023-06-13 12:23:45','2023-06-13 13:23:45')");
-        $dbh->query("INSERT INTO manatime_equipment (name,category,number,description,created_at,updated_at)VALUES ('mouse',              'input device',     'zx5ggtg5',    'given to Marie jo',        '2023-06-13 13:23:45','2023-06-14 13:23:45')");
-        $dbh->query("INSERT INTO manatime_equipment (name,category,number,description,created_at,updated_at)VALUES ('screen',             'display device',   'hyy656565',   'damaged unrepairable',     '2023-06-13 14:23:45','2023-06-15 13:23:45')");
-        $dbh->query("INSERT INTO manatime_equipment (name,category,number,description,created_at,updated_at)VALUES ('laptop',             'input device',     '09809807jh',  'reported malfunc,untested','2023-06-13 15:23:45','2023-06-16 13:23:45')");
-        $dbh->query("INSERT INTO manatime_equipment (name,category,number,description,created_at,updated_at)VALUES ('removable hard disk','input device',     'hkhjkgyt987', 'damaged by sanjeev',       '2023-06-13 16:23:45','2023-06-17 13:23:45')");
-
-        //$sth=$dbh->query("DELETE FROM manatime_equipment");
-
-        // fetch all rows into array, by default PDO::FETCH_BOTH is used
-        //$rows = $sth->fetchAll();
-    }
-
-    public function testAPI(): void
+    public function testEquipmentRoute3(): void
     {
         //127.0.0.1:8001/equipment/delete/
         $client = new \GuzzleHttp\Client([
@@ -109,4 +105,9 @@ class SpamCheckerTest extends TestCase
         echo ($response->getBody() . "\n");
         $this->assertTrue(true);
     }
+
+
+
+
+    
 }
